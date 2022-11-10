43d9514b42d5bbc37f81a9d94eb276f7080de7dc
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-11-10 20:10:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
-float a=0;
-float b=0;
-float c=0;
-cin >> a >> b >> c;  
-//Qui va scritto il codice per eseguire i calcoli richiesti
-cout<<"l'area del triangolo vale: "<< endl;
-cout<<(a*b)/2<<endl;
-cout<<"l'area del quadrato vale: "<< endl;
-cout<<(a*a)<<endl;
-cout<<"l'area del rettangolo vale: "<< endl;
-cout<<(a*b)<<endl;
-cout<<"l'area del trapezio vale: "<< endl;
-cout<<((a+b)*c)/2<<endl;
-return 0;
-}  
+int main()
+{
+    float a, b, c;
+    cin >> a >> b >> c;
+    cout << (a*b)/2 << endl;
+    cout << a*a << endl;
+    cout << a*b << endl;
+    cout << ((b+a)*c)/2 << endl;
+    return 0;
+}
